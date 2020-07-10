040ae8885ccefe4f07d7b877b02175902fa362e4
Derek Fitzpatrick <dfitz.murrieta@gmail.com>
2020-07-10 15:40:53 -0700

    add management argument option for smurfs

--- api/management/commands/smurfs.py
+++ api/management/commands/smurfs.py
@@ -8,8 +8,11 @@ log = logging.getLogger('zcl.api.management')
 
 class Command(BaseCommand):
 
+    def add_arguments(self, parser):
+        parser.add_argument('discord_id', type=str, help='Discord ID')
+
     def handle(self, *args, **options):
-        discord_id = str(args[0])
+        discord_id = options['discord_id']
         user = DiscordUser.objects.get(id=discord_id)
         for p in user.profiles.all():
             print(p)
