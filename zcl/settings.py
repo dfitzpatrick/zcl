51921d7d64f0125d24e7174585a65f58c35163df
Derek Fitzpatrick <dfitz.murrieta@gmail.com>
2020-12-28 03:16:44 -0800

    sdg

--- zcl/settings.py
+++ zcl/settings.py
@@ -92,18 +92,20 @@ SITE_URL = config('SITE_URL')
 PUBLIC_SITE_URL = config('PUBLIC_SITE_URL')
 
 FRONTEND = "http://localhost:8000/"
-
+ALLOWED_HOSTS=['*']
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:8000",
     "http://localhost:8000",
     "http://localhost:3000",
     "https://localhost:3000",
-    "http://127.0.0.1:3000"
+    "http://127.0.0.1:3000",
+    'http://zclbeta.com',
+    'http://zclleagues.com'
 ]
 CORS_ORIGIN_WHITELIST = CORS_ALLOWED_ORIGINS
 CORS_ALLOW_CREDENTIALS = True
 
-CSRF_TRUSTED_ORIGINS = ['localhost:3000']
+CSRF_TRUSTED_ORIGINS = ['localhost:3000', 'zcleagues.com', 'zclbeta.com']
 
 
 """
